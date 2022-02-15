6d627f452ef073fe3b94b903e22535d121b110a7
dev-ys-36 <98698629+dev-ys-36@users.noreply.github.com>
2022-02-16 00:50:55 +0900

    Add files via upload

---
+++ testC.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Test
+{
+private:
+	string addresss;
+public:
+	void ShowInfo(string address);
+};
+
+void Test::ShowInfo(string address)
+{
+	char cmd[100];
+	sprintf_s(cmd, "ping %s > %s.txt", address.c_str(), address.c_str());
+	system(cmd);
+}
+
+void main()
+{
+	string address;
+	Test test;
+	cout << "IP or Domain: ";
+	cin >> address;
+	test.ShowInfo((string)address);
+}
